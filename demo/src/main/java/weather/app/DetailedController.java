19d0987030d486b22893add212d8b1b9b317fa34
Brace <11965313+DoctorBracewell@users.noreply.github.com>
2024-05-15 12:15:37 +0100

    changes

--- demo/src/main/java/weather/app/DetailedController.java
+++ demo/src/main/java/weather/app/DetailedController.java
@@ -1,12 +1,24 @@
 package weather.app;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class DetailedController {
+    @FXML
+    private Text clock;
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    private void initialize() throws IOException {
+
+    }
+
+    @FXML
+    private void switchToHome() throws IOException {
         App.setRoot("home");
     }
 }
