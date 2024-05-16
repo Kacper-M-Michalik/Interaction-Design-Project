94a3dcc1a26dcc1934dae997d9a1d4b71a6db2cb
Brace <11965313+DoctorBracewell@users.noreply.github.com>
2024-05-16 15:14:03 +0100

    Update HomeController.java

--- demo/src/main/java/weather/app/HomeController.java
+++ demo/src/main/java/weather/app/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 
     @FXML
     private void initialize() {
-        temperature.setText(WeatherAndLocationManager.WeatherData.getString("elevation") + "°C");
+
     }
 
     @FXML
